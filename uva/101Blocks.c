3ffb303438cc803a13eb1d7d2602b82e2a3f1190
cyyuen <colsy2@gmail.com>
2014-12-16 14:33:31 +0800

    [101] improve by storing stack information

--- uva/101Blocks.c
+++ uva/101Blocks.c
@@ -4,6 +4,7 @@
 
 typedef struct block {
 	int id;
+	int stk;
 	struct block *up, *down;
 } block;
 
@@ -13,8 +14,14 @@ block positions[MAX_B];
 void push(block* a, block* b) {
 	if (a->down)
 		a->down->up = NULL;
+	positions[b->stk].down = positions[a->stk].down;
+	positions[a->stk].down = a->down;
 	a->down = b;
 	b->up = a;
+	while(a) {
+		a->stk = b->stk;
+		a = a->up;
+	}
 }
 
 void blocks_reset(int num) {
@@ -28,23 +35,25 @@ void blocks_init() {
 	int i;
 	for (i=0; i !=MAX_B; ++i) {
 		blocks[i].id = i;
+		blocks[i].stk = i;
+		positions[i].stk = i;
+		positions[i].down = &blocks[i];
+		positions[i].up = &blocks[i];
+		blocks[i].down = &positions[i];
 	}
-	blocks_reset(MAX_B);
 }
 
 block* stack_top(block* bk) {
-	block* b = bk;
-	while (b->up) b = b->up;
-	return b;
+	return positions[bk->stk].down;
 }
 
 void clean_top(block* bk) {
 	block* b = bk->up;
-
 	while(b) {
 		push(b, &positions[b->id]);
 		b = b->up;
 	}
+	positions[bk->stk].down = bk;
 }
 
 void showblock(int num) {
@@ -85,7 +94,7 @@ int main(int argc, char const *argv[])
 		while(scanf("%s", cmd) != EOF && cmd[0] != 'q') {
 			char c1 = cmd[2];
 			scanf("%d %s %d", &id1, cmd, &id2);
-			if (id1 != id2 && stack_top(&blocks[id1]) != stack_top(&blocks[id2]))
+			if (id1 != id2 && blocks[id1].stk != blocks[id2].stk)
 				push(get_block(c1, id1), get_block(cmd[2], id2));
 		}
 		showblock(num);
